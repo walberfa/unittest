1f6231f46ad13fd6c8a496b65d98d0abdf64b79b
Walber Florencio <walberfa@gmail.com>
2022-10-20 01:00:42 -0300

    Refatorando test_calculadora e add docstrings em calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,22 +1,49 @@
+"""Calculadora simples com as quatro operações básicas.
+"""
+
 
 def adicao(x, y):
-    # Calculo de adição
+    """Calculo de adição.
+    Args:
+        x (float): Primeiro parâmetro da soma.
+        y (float): Segundo parâmetro da soma.
+    Return:
+        Resultado da soma.
+    """
     return x + y
 
 
 def subtracao(x, y):
-    # Calculo de subtração
+    """Calculo de subtração.
+    Args:
+        x (float): Primeiro parâmetro da subtração.
+        y (float): Segundo parâmetro da subtração.
+    Return:
+        Resultado da subtração.
+    """
     return x - y
 
 
 def multiplicacao(x, y):
-    # Calculo de divisão
+    """Calculo de multiplicação.
+    Args:
+        x (float): Primeiro parâmetro da multiplicação.
+        y (float): Segundo parâmetro da multiplicação.
+    Return:
+        Resultado da multiplicação.
+    """
     return x * y
 
 
 def divisao(x, y):
-    # Calculo de divisão
+    """Calculo de divisão.
+    Args:
+        x (float): Primeiro parâmetro da divisão.
+        y (float): Segundo parâmetro da divisão. Tem que ser diferente de zero.
+    Return:
+        Resultado da divisão.
+    """
     if y == 0:
-        raise ValueError("Não pode realizar divisão por zero")
+        raise ZeroDivisionError("Não pode realizar divisão por zero")
     return x / y
 
--- test_calculadora.py
+++ test_calculadora.py
@@ -3,43 +3,51 @@ import calculadora
 
 
 class TestCalculadora(unittest.TestCase):
+    """Teste para uma calculadora simples.
+    """
 
     def test_adicao(self):
-        # Soma com dois números positivos
-        self.assertEqual(calculadora.adicao(13, 13), 26)
-        # Soma com dois números negativos
-        self.assertEqual(calculadora.adicao(-3, -6), -9)
-        # Soma com um número positivo e um negativo
-        self.assertEqual(calculadora.adicao(2, -1), 1)
+        """Testando a função soma.
+        """
+        with self.subTest("utilizando argumentos válidos"):
+            self.assertEqual(calculadora.adicao(13, 13), 26)
+
+        with self.subTest("forçando exceção inserindo parâmetro inválido"):
+            with self.assertRaises(TypeError):
+                self.assertEqual(calculadora.adicao(2, "a"), -4)
 
     def test_subtracao(self):
-        # Subtração com dois números positivos
-        self.assertEqual(calculadora.subtracao(12, 6), 6)
-        # Subtração com um número positivo e um negativo
-        self.assertEqual(calculadora.subtracao(-2, 2), -4)
-        # Subtração com um dois números negativos
-        self.assertEqual(calculadora.subtracao(-3, -3), 0)
+        """Testando a função subtração.
+        """
+        with self.subTest("utilizando argumentos válidos"):
+            self.assertEqual(calculadora.subtracao(12, 6), 6)
+
+        with self.subTest("forçando exceção inserindo parâmetro inválido"):
+            with self.assertRaises(TypeError):
+                self.assertEqual(calculadora.subtracao(-2, "2"), -4)
 
     def test_multiplicacao(self):
-        # Multiplicação com zero
-        self.assertEqual(calculadora.multiplicacao(0, 5), 0)
-        # Multiplicação com número negativo e com 1
-        self.assertEqual(calculadora.multiplicacao(-1, 6), -6)
-        # Multiplicação com dois números negativos
-        self.assertEqual(calculadora.multiplicacao(-2, -5), 10)
+        """Testando a função multiplicação.
+        """
+        with self.subTest("utilizando argumentos onde um deles é zero"):
+            self.assertEqual(calculadora.multiplicacao(0, 5), 0)
+
+        with self.subTest("utilizando argumentos com número negativo"):
+            self.assertEqual(calculadora.multiplicacao(-1, 6), -6)
 
     def test_divisao(self):
-        # Divisão com números positivos
-        self.assertEqual(calculadora.divisao(10, 2), 5)
-        # Divisão com número negativo e com 1
-        self.assertEqual(calculadora.divisao(-3, 1), -3)
-        # Divisão com resultado não inteiro
-        self.assertEqual(calculadora.divisao(1, 2), 0.5)
+        """Testando a função subtração.
+        """
+        with self.subTest("utilizando argumentos válidos"):
+            self.assertEqual(calculadora.divisao(10, 2), 5)
 
-    def test_divisao_por_zero(self):
-        # Exceção para divisão com zero
-        with self.assertRaises(ValueError):
-            calculadora.divisao(2, 0)
+        with self.subTest("forçando exceção inserindo parâmetro inválido"):
+            with self.assertRaises(TypeError):
+                self.assertEqual(calculadora.divisao("1", 1), 1)
+
+        with self.subTest("forçando uma exceção inserindo o zero no parâmetro y"):
+            with self.assertRaises(ZeroDivisionError):
+                calculadora.divisao(2, 0)
 
 
 if __name__ == '__main__':
